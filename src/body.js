fb897cf571394e6bc50971723c8ba738b27895e4
Tushar Dupga <Tushardupga@gmail.com>
2023-04-17 23:32:20 +0530

    final commit

--- src/body.js
+++ src/body.js
@@ -1,44 +1,52 @@
-import React, { useState } from "react";
+import { useState, useEffect } from 'react';
 import './body.css';
-
+import axios from 'axios';
 
 function Body() {
-	const [isLoading, setIsLoading] = useState(true);
 	const logo = require('../src/placeholder1.png')
+	const [response, setResponse] = useState(null);
+	const [file, setFile] = useState(logo);
+	const [uploadFile, setUploadFile] = useState(null)
+	const [isLoading, setIsLoading] = useState(true);
+
 
   function onLoad() {
-    // delay for demo only
-
-   	setIsLoading(!isLoading)
-	console.log(isLoading)
+	var formData = new FormData();
+	formData.append("image", uploadFile);
+	axios.post("https://32c0-2401-4900-5d2d-37b9-bce-350f-9bbd-f2cd.ngrok-fr", formData, {
+		headers: {
+		'Access-Control-Allow-Origin': '*',
+		'Content-Type': 'multipart/form-data'
+		}
+	}).then((response) => {
+		console.log(response)
+		setResponse(response.content)
+		setIsLoading(!isLoading)
+	})
   }
 
-
-	const [file, setFile] = useState(logo);
 	function handleChange(e) {
 		console.log(e.target.files);
 		setFile(URL.createObjectURL(e.target.files[0]));
+		setUploadFile(e.target.files[0])
 	}
-
 	return (
 		<div className="App">
 			<h2>Add Image:</h2>
-			{/* <img
+			<img
         		alt="ad-img"
         		width={300}
-        		src={logo}
-        		// style={{ display: isLoading ? "block" : "none" }}
-      		/> */}
+        		src={file}
+        		style={{ display: isLoading ? "none" : "block" }}
+      		/>
 			<img src={file}
-			// style={{ display: isLoading ? "none" : "block" }}
-			// onLoad={onLoad}
+				style={{ display: isLoading ? "block" : "none" }}
 			/>
-            <input type="file" onChange={handleChange} 
-			/>
+            <input type="file" onChange={handleChange} style={{display: isLoading? "block" : "none"}}/>
 			<button onClick={onLoad} style={{display: isLoading? "block" : "none"}}>Upload Image</button>
-			<div>
+			<div class="left_right">
 				<button style={{ display: isLoading ? "none" : "block" }}>Download</button>
-				<button  style={{ display: isLoading ? "none" : "block" }}>Back</button>
+				<button onClick={onLoad} style={{ display: isLoading ? "none" : "block" }}>Back</button>
 			</div>
 		</div>
 
