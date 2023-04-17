57f714569014f8054bd985fb2d20f08895c1de00
Tushar Dupga <Tushardupga@gmail.com>
2023-04-17 16:14:41 +0530

    Buttons added

--- src/body.js
+++ src/body.js
@@ -1,8 +1,20 @@
 import React, { useState } from "react";
 import './body.css';
 
+
 function Body() {
-	const [file, setFile] = useState();
+	const [isLoading, setIsLoading] = useState(true);
+	const logo = require('../src/placeholder1.png')
+
+  function onLoad() {
+    // delay for demo only
+
+   	setIsLoading(!isLoading)
+	console.log(isLoading)
+  }
+
+
+	const [file, setFile] = useState(logo);
 	function handleChange(e) {
 		console.log(e.target.files);
 		setFile(URL.createObjectURL(e.target.files[0]));
@@ -11,9 +23,23 @@ function Body() {
 	return (
 		<div className="App">
 			<h2>Add Image:</h2>
-			
-			<img src={file} />
-            <input type="file" onChange={handleChange} />
+			{/* <img
+        		alt="ad-img"
+        		width={300}
+        		src={logo}
+        		// style={{ display: isLoading ? "block" : "none" }}
+      		/> */}
+			<img src={file}
+			// style={{ display: isLoading ? "none" : "block" }}
+			// onLoad={onLoad}
+			/>
+            <input type="file" onChange={handleChange} 
+			/>
+			<button onClick={onLoad} style={{display: isLoading? "block" : "none"}}>Upload Image</button>
+			<div>
+				<button style={{ display: isLoading ? "none" : "block" }}>Download</button>
+				<button  style={{ display: isLoading ? "none" : "block" }}>Back</button>
+			</div>
 		</div>
 
 	);
